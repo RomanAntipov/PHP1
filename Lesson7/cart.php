8003e8218f3f9fd8e9f8171d720308164c0544af
RomanAntipov <antipov.r@gmail.com>
2019-03-07 00:13:46 +0300

    Добавил логику отображения содержимого корзины на основании хранящихся в SESSION данных об ID и количестве.

--- Lesson7/cart.php
+++ Lesson7/cart.php
@@ -2,33 +2,38 @@
 
 $link = mysqli_connect('localhost:3307', 'root', '', 'webshop');
 
-$products = mysqli_query($link, "SELECT * FROM webshop.products;");
+$allprod = mysqli_query($link, "SELECT * FROM webshop.products;");
 // var_dump($products);
 
 // setcookie('name', 'mycart', time() + 3600 * 24 * 7);
-session_start();
 
-$productslist = [];
-while ($row = mysqli_fetch_assoc($products)) {
+
+$products = [];
+while ($row = mysqli_fetch_assoc($allprod)) {
     // var_dump($row);
-    $productslist[] = $row;
-}
-
-// проверяем, что функция отработала и массив сформирован, в продуктовой версии данную строчку закомментировать.
-// var_dump($productslist);
-
-$prodtags = '';
-foreach ($productslist as $item) {
-    // тест - проверяем, как функция перебирает значения в массиве. В продуктовой версии строчку закомментировать.
-    // echo 'Ключ: '.$id.', имя файла: '.$item['id'].'<br/>'.$item['product_name'].'<br/>'.$item['prod_image'].'<br/>';
-    if (!isset($_SESSION[$item['id']])) $_SESSION[$item['id']] = 0;
-    $prodname = $item['product_name'];
-    $currentitem = '<div><img src="'.$item['prod_image'].'" alt="'.$prodname.'" title="'.$prodname.'"><br><h2>'.$prodname.'</h2><p>Price: $'.$item['price'].'</p><p><form method="get"><input type="number" name="'.$item['id'].'" class="input"><button type="submit">Купить</button></form></p></div>';
-    
-    $prodtags .= $currentitem;
+    $products[] = $row;
 };
 
+// проверяем, что функция отработала и массив сформирован, в продуктовой версии данную строчку закомментировать.
+// var_dump($products);
+
 // var_dump($_SESSION);
+
+$prodtags = '';
+$prodtable = [];
+foreach ($products as $item) {
+    // тест - проверяем, как функция перебирает значения в массиве. В продуктовой версии строчку закомментировать.
+    // echo 'Ключ: '.$id.', имя файла: '.$item['id'].'<br/>'.$item['product_name'].'<br/>'.$item['prod_image'].'<br/>';
+    // echo $_SESSION[$item['id']].'<br>';
+    $prodname = $item['product_name'];
+    $id = $item['id'];
+    if (!isset($_SESSION[$id])) $_SESSION[$id] = 0;
+    $currentitem = '<div><img src="'.$item['prod_image'].'" alt="'.$prodname.'" title="'.$prodname.'"><br><h2>'.$prodname.'</h2><p>Price: $'.$item['price'].'</p><p><form method="get"><input type="number" name="'.$id.'" class="input"><button type="submit">Купить</button></form></p></div>';
+    $prodtags .= $currentitem;
+    $prodtable [$id] = ['prodname' => $prodname, 'price' => $item['price']];  // формируем двумерный массив, в котором ключи ячеек равны id, вложенные массивы содержат инфо о наименовании и цене товара.
+};
+
+// var_dump($prodtable);
 
 mysqli_close($link);
 
@@ -39,5 +44,12 @@ mysqli_close($link);
 foreach ($_GET as $id => $count) {
 	$_SESSION[$id] += $count;
 };
-var_dump($_SESSION); 
+// var_dump($_SESSION); 
+
+foreach ($_SESSION as $id => $count) {
+	if ($count > 0)
+	echo $id.' - '.$prodtable[$id]['prodname'].' - количество:'.$count.' шт., цена: '.$prodtable[$id]['price'].', сумма: '.$prodtable[$id]['price']*$count;
+};
+
+
 ?>
